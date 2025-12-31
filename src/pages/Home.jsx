cbda1bc557027ad687c0d873441d8ce8f6c24f0e
Akpere38 <akpereraphael@gmail.com>
2025-12-31 15:50:10 +0100

    Fix import case sensitivity for Render

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import MyNavbar from '../components/navbar';
+import MyNavbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
